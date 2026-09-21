19a0cd0d273eca41dc38d1cb36ebac8cf49e75a2
Prathik111 <imnoobinall@gmail.com>
2026-09-21 20:15:25 +0530

    fix: clear registry client clippy lints

--- src-tauri/src/registry.rs
+++ src-tauri/src/registry.rs
@@ -179,6 +179,7 @@ impl RegistryClient {
 
 
     #[allow(clippy::too_many_arguments)]
+    #[allow(clippy::too_many_arguments)]
     pub(crate) async fn create_model(
         &self,
         id: &str,
